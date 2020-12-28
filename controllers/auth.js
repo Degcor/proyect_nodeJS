980bc21826d8a9c914f03236eb242241e777e422
degcor <diego.rebeck@gmail.com>
2020-12-28 15:10:34 -0300

    prueba

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,7 +26,7 @@ exports.signin = (req, res) => {
     User.findOne({ email }, (err, user) => {
         if (err || !user) {
             return res.status(400).json({
-                error: "User with that email does not exist. Please signup"
+                error: "User with that email does not exist. Please Signup"
             });
         }
         // if user is found make sure the email and password match
